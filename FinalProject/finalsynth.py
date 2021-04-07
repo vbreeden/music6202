db1cb94fe190d2a37ec5be17ea1eece32c02f34c
Virgil <virgil.breeden@gmail.com>
2021-04-07 11:50:02 -0400

    Added the remaining commandline argument flags

--- FinalProject/finalsynth.py
+++ FinalProject/finalsynth.py
@@ -18,11 +18,26 @@ parser = argparse.ArgumentParser()
 # Custom arguments are added here. By default, argparse provides a Help argument. It can be accessed using:
 # python finalsynth.py -h
 def define_args():
+    # Modulation effects
+    parser.add_argument('-c', '--chorus', nargs='+', action='append', help='Add a chorus effect to the signal path.'
+                                                                           'Ex: finalsynth -c ChorusParam')
+    parser.add_argument('-d', '--delay', nargs='+', action='append', help='Add a delay effect to the signal path.'
+                                                                          'Ex: finalsynth -d DelayParam')
+    parser.add_argument('-v', '--vibrato', nargs='+', action='append', help='Add a chorus vibrato to the signal path.'
+                                                                            'Ex: finalsynth -v VibratoParam')
+
+    # Filters
+    parser.add_argument('-b', '--bandpass', nargs='+', action='append', help='Add a bandpass filter to the signal path.'
+                                                                             'Ex: finalsynth -b 400Hz 500Hz')
     parser.add_argument('-l', '--lowpass', nargs='+', action='append', help='Add a lowpass filter to the signal path.'
                                                                             'Ex: finalsynth -l 100Hz')
+
+    # Convolution effects
     parser.add_argument('-r', '--reverb', nargs='+', action='append', help='Add a reverb effect to the signal path '
                                                                            'that utilizes a provided impulse response '
                                                                            'wav file. Ex: finalsynth -r IR.wav')
+
+    # Kern files
     parser.add_argument('-k', '--kern', nargs='+', action='append', help='Provide the name of the input file in kern '
                                                                          'format. Ex: finalsynth -k melody1.krn')
     return parser.parse_args()
