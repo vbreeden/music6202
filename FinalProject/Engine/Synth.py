f4be7651a3a09f80ab4645c2ffea123129601050
Virgil <virgil.breeden@gmail.com>
2021-04-11 22:43:27 -0400

    I fixed a line spacing issues that my IDE was highlighting as well as added a touch of code cleaning. Functionally, nothing has changed.

--- FinalProject/Engine/Synth.py
+++ FinalProject/Engine/Synth.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass, field
 from scipy.io.wavfile import write
-from scipy import signal
 
 import numpy as np
 
@@ -12,7 +11,7 @@ class AdditiveSynth:
     wave_type: str = ''
     wave: list[float] = field(default_factory=list)
 
-    def generate_additive_wav(self, notes, wave_file_path='out_square', wave_type = 'square'):
+    def generate_additive_wav(self, notes, wave_file_path='out_square', wave_type='square'):
         self.wave_file_path = wave_file_path
         self.wave_type = wave_type
         frequencies = notes.frequencies
@@ -20,30 +19,31 @@ class AdditiveSynth:
         durations = notes.durations
 
         for i in range(len(frequencies)):
-        	print("*** i=", i, "***")
-        	if(frequencies[i] != 0):
-        		if (self.wave_type == 'sine'):
-	        		self.wave.extend(self.generate_sine_wave(frequencies[i], durations[i], amplitudes[i]))
-	        	elif (self.wave_type == 'square'):
-	        		self.wave.extend(self.generate_square_wave(frequencies[i], durations[i], amplitudes[i]))
-	        	
+            print("*** i=", i, "***")
+            if frequencies[i] != 0:
+                if self.wave_type == 'sine':
+                    self.wave.extend(self.generate_sine_wave(frequencies[i], durations[i], amplitudes[i]))
+                elif self.wave_type == 'square':
+                    self.wave.extend(self.generate_square_wave(frequencies[i], durations[i], amplitudes[i]))
+
         write(self.wave_file_path + ".wav", SAMPLE_RATE, np.array(self.wave))
 
-    def get_single_phase_argument(self,frequency):
-    	single_cycle_length = SAMPLE_RATE / float(frequency)
-    	omega = np.pi * 2 / single_cycle_length
-    	phase_array = np.arange(0, int(single_cycle_length)) * omega
-    	return phase_array
+    def get_single_phase_argument(self, frequency):
+        single_cycle_length = SAMPLE_RATE / float(frequency)
+        omega = np.pi * 2 / single_cycle_length
+        phase_array = np.arange(0, int(single_cycle_length)) * omega
+        return phase_array
     
-    def generate_sine_wave(self,frequency, amplitude, duration):
-    	num_samples = int(duration * SAMPLE_RATE)
-    	phase_array = self.get_single_phase_argument(frequency)
-    	single_cycle = amplitude * np.sin(phase_array)
-    	resized_single_cycle = np.resize(single_cycle, (num_samples)).astype(np.float)
-    	return resized_single_cycle
+    def generate_sine_wave(self, frequency, amplitude, duration):
+        num_samples = int(duration * SAMPLE_RATE)
+        phase_array = self.get_single_phase_argument(frequency)
+        single_cycle = amplitude * np.sin(phase_array)
+        resized_single_cycle = np.resize(single_cycle, num_samples).astype(np.float)
+        return resized_single_cycle
 
     def generate_square_wave(self, frequency, amplitude, duration):
-    	return amplitude * np.sign(self.generate_sine_wave(frequency, amplitude, duration))
+        return amplitude * np.sign(self.generate_sine_wave(frequency, amplitude, duration))
+
 
 @dataclass
 class WavetableSynth:
