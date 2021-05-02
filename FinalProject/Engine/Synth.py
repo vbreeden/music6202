2da2237cf3f2a58b36522fc870d0b4f6c4794483
Virgil <virgil.breeden@gmail.com>
2021-05-02 18:45:02 -0400

    Fixed whatever broke in the synthesizer

--- FinalProject/Engine/Synth.py
+++ FinalProject/Engine/Synth.py
@@ -94,10 +94,13 @@ class AdditiveSynth:
         # rescale to prevent clipping
         # if num_harmonics > 1:
         # x = sumSines / (numHarmonics - 1)
-        if max(sum_sines) != 0:
-            x = amplitude * sum_sines/max(sum_sines)
-        else:
-            x = np.zeros(len(sum_sines))
+
+        # Condition to cater to the divide by zero case
+        flag = max(sum_sines) if max(sum_sines) else 1
+
+        x = amplitude * np.divide(sum_sines, flag)
+        x = x * np.iinfo(np.int32).max
+        x = x.astype(np.int32)
 
         return x
 
@@ -116,19 +119,18 @@ class WavetableSynth:
 
     def generate_wavetable(self,  notes, timbre, sweep, speed):
         self.wave_file_path = 'out_wavetable'
-        frequencies = np.around(notes.frequencies,2)
+        frequencies = notes.frequencies
         amplitudes = notes.amplitudes
         durations = notes.durations
 
-       # print('frequencies=', frequencies)
-       # print('amplitudes=', amplitudes)
-       # print('durations=', durations)
+        print('frequencies=', frequencies)
+        print('amplitudes=', amplitudes)
+        print('durations=', durations)
 
         # get list of unique frequencies in the kern file
         unique_freqs = np.unique(frequencies)
-        #remove frequency of 0 from the list (placeholder for rests) - do not need to include in the wavetable
-        unique_freqs=unique_freqs[unique_freqs!=0]
-        #print('unique_freqs=',unique_freqs)
+        unique_freqs = unique_freqs[unique_freqs != 0]
+        # print('unique_freqs=',unique_freqs)
         wavetable = []
         # first dimension of wavetable corresponds to number of unique frequencies in the krn file (excluding rests)
         for i in range(len(unique_freqs)):
@@ -205,20 +207,14 @@ class WavetableSynth:
                 x_rest = np.zeros(num_samples_note, dtype='float32')
                 x = self.wave.extend(x_rest)
 
-        plt.plot(self.wave)
-        plt.savefig('wavetableplot.jpg')
-        plt.close()
-        plt.plot(self.wave[336000:338000])
-        plt.savefig('wavetableplot_subset.jpg')
-        plt.close()
-        plt.plot(self.wave[8000:10000])
-        plt.savefig('wavetableplot_subset2.jpg')
-        plt.close()
-        plt.plot(self.wave[47000:49000])
-        plt.savefig('wavetableplot_subset3.jpg')
-        plt.close()
+        self.wave = np.asarray(self.wave, np.float64)
 
-        write(self.wave_file_path + ".wav", SAMPLE_RATE, np.array(self.wave))
+        #The following lines amplify the amplitudes proportionately to int32
+        # Condition to cater to the divide by zero case
+        flag = max(self.wave) if max(self.wave) else 1
+        self.wave = 0.5 * np.divide(self.wave, flag)
+        self.wave = self.wave * np.iinfo(np.int32).max
+        self.wave = self.wave.astype(np.int32)
 
         return self.wave
 
